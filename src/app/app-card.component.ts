4d0531ef1b4cd147068084a64c2beb91211942a8
Connie Leung <cleung334@gmail.com>
2024-07-01 22:16:06 +0800

    feat: content projection fallback

---
+++ src/app/app-card.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  template: `
+    <div class="header">
+      <ng-content select="[header]">Free Tier</ng-content>
+    </div>
+    <div class="content">
+      <ng-content> 
+        <ul>
+          <li>Free of Charge</li>
+          <li>1 License</li>
+          <li>500MBs Storage</li>
+          <li>No Support</li>
+        </ul> 
+      </ng-content>
+    </div>
+    <div class="footer">
+      <ng-content select="[footer]">
+        <button>Upgrade</button>
+      </ng-content>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppCardComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,61 @@
 import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
+import { AppCardComponent } from './app-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [AppCardComponent],
   template: `
-    <div>
-      <h1>Content Projection Fallback</h1>
+    <header>Angular {{version}} - Content Projection fallback </header>
+    <main>
+      <section>
+        <h2>Fallback content</h2>
+        <app-card />
+      </section>
 
-    </div>
+      <section>
+        <h2>Custom Content</h2>
+        <app-card>
+          <div header>Start-up</div>
+          <ul>
+            <li>USD 10/month</li>
+            <li>3 Licenses</li>
+            <li>1GB Storage</li>
+            <li>Email Technical Support</li>
+          </ul>
+        </app-card>
+      </section>
+
+      <section>
+        <h2>Custom Content</h2>
+        <app-card>
+          <div header>Company</div>
+          <ul>
+            <li>USD 100/month</li>
+            <li>50 Licenses</li>
+            <li>20GB Storage</li>
+            <li>Email and Phone Technical Support</li>
+            <li>95% Uptime</li>
+          </ul>
+        </app-card>
+      </section>
+
+      <section>
+        <h2>Custom Content</h2>
+        <app-card>
+          <div header>Enterprise</div>
+          <ul>
+            <li>USD 999/month</li>
+            <li>200+ Licenses</li>
+            <li>1TB Storage</li>
+            <li>Email and Phone Technical Support</li>
+            <li>Dedicated Support Staff</li>
+            <li>99.999% Uptime</li>
+          </ul>
+          <div footer>&nbsp;</div>
+        </app-card>
+      </section>
+    </main>
   `,
   styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
